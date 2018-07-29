26b4c1b858a22f2755b2c4eda2426fe2a238d5f5
NikitaBolsunovskiy <41104234+NikitaBolsunovskiy@users.noreply.github.com>
2018-07-29 17:20:48 +0300

    Update

--- main.c
+++ main.c
@@ -76,23 +76,26 @@ void insert(Node **head, T value){
 
 void preOrderTravers(Node *root){
     if (root){
-        printf("%d", root->data);
+        printf("%d   ", root->data);
+        fflush(stdout);
         preOrderTravers(root->left);
         preOrderTravers(root->right);
     }
 }
 void inOrderTravers(Node *root){
     if (root){
-        preOrderTravers(root->left);
-        printf("%d", root->data);
-        preOrderTravers(root->right);
+        inOrderTravers(root->left);
+        printf("%d   ", root->data);
+        fflush(stdout);
+        inOrderTravers(root->right);
     }
 }
 void postOrderTravers(Node *root){
     if (root){
-        preOrderTravers(root->left);
-        preOrderTravers(root->right);
-        printf("%d", root->data);
+        postOrderTravers(root->left);
+        postOrderTravers(root->right);
+        printf("%d   ", root->data);
+        fflush(stdout);
     }
 }
 
@@ -127,8 +130,100 @@ void solution1(){
 
 }
 
+void readComandLine(const  char * comandLine, char *filePath, int *orderVar){
+    int i = 0;
+    while (comandLine[i]!= '\0'){
+
+        if (comandLine[i]=='/'){
+            i++;
+            if(comandLine[i]=='f'){
+                i++;
+                i++;
+                int j = 0;
+                while (comandLine[i]!= '/') {
+                    filePath[j] = comandLine[i];
+                    i++;
+                    j++;
+                }
+            } else if (comandLine[i] == 'v'){
+                i++;
+                i++;
+                if (comandLine[i] == '1'){
+                    *orderVar = 1;
+                } else if (comandLine[i] == '2'){
+                    *orderVar = 2;
+                } else if (comandLine[i] == '3'){
+                    *orderVar = 3;
+                } else {
+                    *orderVar = -1;
+                }
+            }
+        } else{
+            i++;
+        }
+    }
+}
+
+Node *getNodeByValue(Node *root, T value) {
+    while (root) {
+        if (root->data > value) {
+            root = root->left;
+            continue;
+        } else if (root->data < value) {
+            root = root->right;
+            continue;
+        } else {
+            return root;
+        }
+    }
+    return NULL;
+}
+
 void solution2(){
 
+    char filePath[] = "";
+    int orderVar = 0;
 
+    printf("Введите команду для чтения файла с данными двоичного дерева и порядком обхода. \n");
+    printf("путь к файлу: [Полный путь к файлу с расширением].\n");
+    scanf("%s",filePath);
+    printf("Вариант обхода: [вариает обхода: 1 - pre-order, 2 - in-order, 3 - post-order].\n");
+    scanf("%d",&orderVar);
 
+    fflush(stdout);
+//    printf("FilePath: %s\n",filePath);
+//    printf("Order variant: %d\n",orderVar);
+
+    Node *Tree = NULL;
+    FILE *file = fopen(filePath,"r");
+    if (file==NULL) {
+        puts("Не удалось открыть Файл!");
+        exit(1);
+    }
+    int count;
+    fscanf(file,"%d",&count);
+    int i;
+    for (i = 0; i < count; ++i) {
+        int value;
+        fscanf(file,"%d",&value);
+        insert(&Tree,value);
+    }
+    fclose(file);
+    printTree(Tree);
+    printf("\nОбход: \n");
+    if (orderVar == 1){
+        preOrderTravers(Tree);
+    } else if (orderVar == 2){
+        inOrderTravers(Tree);
+    } else if (orderVar == 3) {
+        postOrderTravers(Tree);
+    }
+
+    Node * tmp = getNodeByValue(Tree,-7);
+
+    if (tmp == NULL) {
+        printf("\nне найден элемент.\n");
+    } else {
+        printf("\nнайденный элемент: %d\n",tmp->data);
+    }
 }
